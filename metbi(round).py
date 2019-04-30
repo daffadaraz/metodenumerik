e37baade5b7addabe5667466b65994bc2155f160
r0eba <33430017+daffadaraz@users.noreply.github.com>
2019-04-30 22:08:45 +0700

    -Added rounding for each -Fixing Regula Falsi code -Optimizing code

--- metbi(round).py
+++ metbi(round).py
@@ -15,6 +15,8 @@ def main():
 	print("Metode Bisection")
 	print("Persamaan, F(X) = x^3 + 2x^2 - 4x - 4") #jangan lupa ubah ini nya.
 	print("")
+	print("(Berapa angka dibelakang koma)")
+	rou = int(input("Pembulatan : "))
 	err = float(input("Masukkan Error : "))
 
 	#Check apabila Fx1*Fx2
@@ -22,11 +24,13 @@ def main():
 		print("")
 		x1 = float(input("Masukkan X1 : "))
 		x2 = float(input("Masukkan X2 : "))
+		fx1 = round(f(x1),rou)
+		fx2 = round(f(x2),rou)
 		print("")
-		print("Nilai dari, F(%d) = %8.3f " % (x1,f(x1)))
-		print("Nilai dari, F(%d) = %8.3f " % (x2,f(x2)))
+		print("Nilai dari, F(%d) = %8.3f " % (x1,fx1))
+		print("Nilai dari, F(%d) = %8.3f " % (x2,fx2))
 		print("")
-		check = f(x1)*f(x2)
+		check = fx1*fx2
 		if check<0:
 			print("Syarat sudah ok.")
 			break
@@ -41,27 +45,29 @@ def main():
 	print("______________________________________________________")
 
 	cekerr = err + 1;
+	cektemp = cekerr + 1;
 	#Cek apabila taraf error sudah sesuai dengan keinginan.
 	while cekerr > err:
 		n = n + 1
-		x3 = (x1 + x2)/2
-		cekerr = abs(f(x3))
+		x3 = round(((x1 + x2)/2),rou)
+		fx3 = round(f(x3),rou)
+		cekerr = abs(fx3)
 
-		if f(x1) * f(x3) > 0:
+		if fx1 * fx3 > 0:
 			x1 = x3
 		else :
 			x2 = x3
 
 		#OUTPUT
-		print("%3d|     %.8f      %10.8f      %12.10f" % (n,x3,f(x3),cekerr))
+		print("%3d|     %.8f      %10.8f      %12.10f" % (n,x3,fx3,abs(fx3)))
 
-		if cekerr <= err :
+		if cekerr <= err or cekerr == cektemp :
 			print("_____________________")
-			print("Akar Persamaan, %.20f"%(x3))
-			print("Atau ~ %.4f"%(round(x3,4)))
-			print("Error, %.4f"%(round(cekerr,4)))
+			print("Akar Persamaan, %f"%(round(x3,rou)))
+			print("Error, %.4f"%(round(cekerr,rou)))
 			break
-
+			
+		cektemp = cekerr
 		if n%10 == 0:
 			input("lanjut?")
 
--- regfals(round).py
+++ regfals(round).py
@@ -14,19 +14,23 @@ def main():
     print("Regula Falsi")
     print("Persamaan, F(x) = x^3 + 2x^2 - 4x -4") #Jangan lupa edit
     print("")
+    print("(Berapa angka dibelakang koma)")
+    rou = int(input("Pembulatan : "))
     err = float(input("Masukkan Error : "))
 
     while check > 0: #Menentukan X1 dan X2
     	print("")
     	x1 = float(input("Masukkan X1 : "))
     	x2 = float(input("Masukkan X2 : "))
+    	fx1 = round(f(x1),rou)
+    	fx2 = round(f(x2),rou)
     	print("")
     	print("Nilai dari, F(%d) = %f " % (x1,f(x1)))
     	print("Nilai dari, F(%d) = %f " % (x2,f(x2)))
     	print("")
-    	check = f(x1)*f(x2)
+    	check = fx1*fx2
     	if check<0:
-    		print("Syarat sudah ok. %d*%d < 0 >> %5.2f<0"%(f(x1),f(x2),check))
+    		print("Syarat sudah ok. %d*%d < 0 >> %5.2f<0"%(fx1,fx2,check))
     		break
     	else :
     		print("Syarat belum ok f(%d)*f(%d) > 0 >> %5.2f>0"%(x1,x2,check))
@@ -42,22 +46,23 @@ def main():
     cektemp = cekerr + 1;
     while cekerr > err: #Iterasi / Pencarian Akar
         n = n + 1
-        x3 = (x1 * f(x2) - x2 * f(x1)) / (f(x2) - f(x1))
-        cekerr = abs(f(x3))
+        x3 = round(((x1 * f(x2) - x2 * f(x1)) / (f(x2) - f(x1))),rou)
+        fx3 = round(f(x3),rou)
+        cekerr = abs(fx3)
 
         #OUTPUT
-        print("%3d|     %.8f      %10.8f      %12.10f" % (n,x3,f(x3),cekerr))
+        print("%3d|     %.8f      %10.8f      %12.10f" % (n,x3,fx3,cekerr))
         if cekerr <= err or cekerr == cektemp :
             print("________________")
-            print("Akar Persamaan, %.20f"%(x3))
-            print("Atau ~ %.4f"%(round(x3,4)))
+            print("Akar Persamaan, %f"%(round(x3,rou)))
             print("Error, %.4f"%(round(cekerr,4)))
             break
 
-        if f(x1) * f(x3) > 0:
+        if fx1 * fx3 > 0:
             x1 = x3
         else :
             x2 = x3
+            
         cektemp = cekerr
         if n%10 == 0:
             input("Lanjut? Tekan enter.")
@@ -70,7 +75,7 @@ if __name__ == '__main__':
 # 2. Hitung nilai f(x1)  dan  f(x2)
 # 3. Tentukan hasil kali tanda bilangan  f(x1) . f(x2)
 #    Bila  f(x1).f(x2)  > 0 , ganti titik x1 dan x2 . Bila  f(x1).f(x2)  < 0  ( berlawanan tanda )
-# 4. Hitung nilai  x3 = (x1 * f(x2) - x2 * f(x1)) / (f(x2) - f(x1))
+# 4. Hitung nilai  xx3 = ((fx1 * x2) - (fx2 * x1)) / (fx1 - fx2)
 # 5. Hitung nilai  f(x3)
 #    Bila nilai f(x3) mendekati nol , maka x3 adalah akar persamaan . Selesai . Bila tidak,
 # 6. Tentukan hasil kali tanda bilangan  f(x1).f(x3)
